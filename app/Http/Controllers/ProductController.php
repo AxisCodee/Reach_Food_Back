67080a91042d78f1a10bedfaff6fcf702a54d642
thalesMworkAcc <thalesmwork942@gmail.com>
2024-05-13 12:22:07 +0300

    fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use App\Helpers\ResponseHelper;
 use App\Http\Requests\StoreProductRequest;
 use App\Models\Product;
-use App\Services\FileService;
 use App\Services\ProductService;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -21,33 +19,34 @@ class ProductController extends Controller
 
     public function store(StoreProductRequest $request)
 
-    {         $result=$this->productService->storeProduct($request);
-
-         // $imagePath = app(FileService::class)->upload($request, 'image');
-        return ResponseHelper::success($result,null,'products created successfully',200);
+    {
+        $result = $this->productService->storeProduct($request);
+        // $imagePath = app(FileService::class)->upload($request, 'image');
+        return ResponseHelper::success($result, null, 'products created successfully', 200);
     }
 
-    public function update(StoreProductRequest $request,Product $product)
+    public function update(StoreProductRequest $request, Product $product)
     {
-        $result=$this->productService->updateProduct($request,$product);
-        return ResponseHelper::success($result,null,'products update successfully',200);
+        $result = $this->productService->updateProduct($request, $product);
+        return ResponseHelper::success($result, null, 'products update successfully', 200);
     }
 
     public function show()
     {
-        $result=$this->productService->showProduct();
-        return ResponseHelper::success($result,null,'products returned successfully',200);
+        $result = $this->productService->showProduct();
+        return ResponseHelper::success($result, null, 'products returned successfully', 200);
     }
+
     public function index(Product $product)
     {
-        $result=$this->productService->indexProduct($product);
-        return ResponseHelper::success($result,null,'products returned successfully',200);
+        $result = $this->productService->indexProduct($product);
+        return ResponseHelper::success($result, null, 'products returned successfully', 200);
     }
 
     public function destroy(Product $product)
     {
-        $result=$this->productService->deleteProduct($product);
-        return ResponseHelper::success($result,null,'products deleted successfully',200);
+        $result = $this->productService->deleteProduct($product);
+        return ResponseHelper::success($result, null, 'products deleted successfully', 200);
     }
 
 }
