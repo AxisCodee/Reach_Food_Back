4429efcd839ff4983de9f6e8121aafccdc690483
thalesMworkAcc <thalesmwork942@gmail.com>
2024-05-22 14:00:51 +0300

    fixes

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function () {
     Route::controller(AuthController::class)->group(function () {
