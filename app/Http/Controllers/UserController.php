6e3ce6e2f7595c3f802130524e0aef6fa5d1bce8
thalesMworkAcc <thalesmwork942@gmail.com>
2024-06-06 12:08:16 +0300

    fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -61,9 +61,8 @@ class UserController extends Controller
         $salesman = User::findOrFail(auth('sanctum')->id());//auth
         $customers = User::whereHas('trips.dates.order', function ($query) use ($salesman) {
             $query->where('salesman_id', $salesman->id);
-        })
-            //->with(['trips.dates.order.customer'])
-            ->get()->toArray();
+        })->where('role', 'customer')->get()->toArray();
+
 
         return ResponseHelper::success($customers);
     }
