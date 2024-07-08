b61b9b019259f72c558fefe87b1bd919a5d1ff98
Mohammad-Alassali <mhdjal0789@gmail.com>
2024-07-08 13:55:01 +0300

    change username error message

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -62,7 +62,7 @@ class CreateUserRequest extends FormRequest
     public function messages()
     {
         return [
-            'user_name.unique' => 'اسم المستخدم موجود مسبقاً',
+            'user_name.unique' => 'عذراً, اسم المستخدم الذي أدخلته موجود بالفعل. يرجى اختيار اسم مستخدم آخر.',
             'image.max' => 'حجم الصورة كبير',
             'trips.*.end_time' => 'وقت انتهاء الرحلة يجب ان يكون بعد وقت البدء',
         ];
