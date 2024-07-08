7b0af2b8e9dbfc18dfa091d386bef0974e02ac10
Mohammad-Alassali <mhdjal0789@gmail.com>
2024-07-08 12:27:01 +0300

    Add scopes to Orders

--- app/Http/Controllers/TripDatesController.php
+++ app/Http/Controllers/TripDatesController.php
@@ -13,20 +13,8 @@ class TripDatesController extends Controller
     {
         $orders = $tripDate
             ->order()
-            ->when($request->input('s'), function (Builder $query, $search) {
-                $query->whereHas('customer', function (Builder $query) use ($search) {
-                    $query->where('name', 'LIKE', "%$search%");
-                });
-            })
-            ->with([
-                'products',
-                'customer' => [
-                    'contacts:id,user_id,phone_number',
-                    'address:id,city_id,area' => [
-                        'city:id,name'
-                    ]
-                ]
-            ])
+            ->search($request->input('s'))
+            ->withForSalesman()
             ->paginate(10);
         return ResponseHelper::success($orders);
     }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -186,20 +186,8 @@ class OrderService
                         $query->whereIn('day', GetDaysNamesAction::handle($request->input('days')));
                     });
             })
-            ->when($request->input('s'), function (Builder $query, $search) use ($request) {
-                $query->whereHas('customer', function (Builder $query) use ($search) {
-                    $query->where('name', 'LIKE', "%$search%");
-                });
-            })
-            ->with([
-                'products',
-                'customer' => [
-                    'contacts:id,user_id,phone_number',
-                    'address:id,city_id,area' => [
-                        'city:id,name'
-                    ]
-                ]
-            ])
+            ->search($request->input('s'))
+            ->withForSalesman()
             ->paginate(10);
         $orders->getCollection()
             ->each(function ($order) {
