ee02338b5d0d0afdfd856d9adb2d4d8d3239534d
Ziad Bani Almargih <ziadbanialmargih@gmail.com>
2024-07-08 13:50:17 +0300

    get only cities have access

--- app/Services/AddressService.php
+++ app/Services/AddressService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Enums\Roles;
 use App\Models\City;
 use App\Models\Country;
 use Illuminate\Database\Eloquent\Collection;
@@ -18,18 +19,40 @@ class AddressService
 
     public function getCountries()
 {
-    return Country::query()
-        ->with(['cities' => function ($query) {
-            $query->whereHas('branch');
-        }])
-        ->get()
-        ->map(function ($country) {
-            $country->cities = $country->cities->filter(function ($city) {
-                return $city->branch;
-            });
-            return $country;
-        })
-        ->toArray();
+    if(auth()->user()['role'] == Roles::SUPER_ADMIN->value)
+        return Country::query()
+            ->with(['cities' => function ($query) {
+                $query->whereHas('branch');
+            }])
+            ->get()
+            ->map(function ($country) {
+                $country->cities = $country->cities->filter(function ($city) {
+                    return $city->branch;
+                });
+                return $country;
+            })
+            ->toArray();
+    elseif (auth()->user()['role'] == Roles::ADMIN->value) {
+        $cityId = auth()->user()['city_id'];
+        return Country::query()
+            ->whereHas('cities', function ($query) use ($cityId) {
+                $query->where('id', $cityId);
+            })
+            ->with(['cities' => function ($query) use ($cityId) {
+                $query->where('id', $cityId)->whereHas('branch');
+            }])
+            ->get()
+            ->map(function ($country) {
+                $country->cities = $country->cities->filter(function ($city) {
+                    return $city->branch;
+                });
+                return $country;
+            })
+            ->toArray();
+    }else{
+        return [];
+    }
+
 }
     public function getCities($country)
     {
